5df16c99438717dc1ef9ca04f99ce344b18d2aec
RutujaPatil18 <prutuja189@gmail.com>
2022-10-26 23:26:30 +0530

    Add: Create employee_payroll service table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -6,3 +6,12 @@ use employee_payroll;
 
 select database();
 
+create table employee_payroll_service(
+id int unsigned auto_increment not null,
+name varchar(50),
+salary double,
+start_date date ,
+primary key(id)
+);
+
+show tables;
