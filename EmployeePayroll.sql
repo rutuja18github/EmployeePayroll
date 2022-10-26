049e2ef63dc5bc9bd0ac90b7cb3da77d7b781e00
RutujaPatil18 <prutuja189@gmail.com>
2022-10-26 23:58:46 +0530

    Add: Retrieve salary Data for particular employee and data for particular date range

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -20,3 +20,10 @@ insert into employee_payroll_service
 (name,salary,start_date) values("Shweta",50000,"2022-10-26"),("Ashish",40000,"2020-11-15"),("Charlie",30000,"2020-05-21");
 
 select * from employee_payroll_service;
+
+insert into employee_payroll_service
+(name,salary,start_date) values("Bill",50000,"2018-01-01");
+
+select salary from employee_payroll_service where name='Bill';
+ 
+select * from employee_payroll_service where start_date between cast('2018-01-01' as date) and Date(now());
